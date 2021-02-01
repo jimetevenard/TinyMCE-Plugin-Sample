6160f5048ff170b1a175a4bf43912e88a9a4aef1
Jim ETEVENARD <j.etevenard-ext@lefebvre-sarrut.eu>
2021-02-01 15:04:50 +0100

    Additional comments

--- darkmode.js
+++ darkmode.js
@@ -25,6 +25,8 @@ tinymce.PluginManager.add('my_plugin_darkmode', function (editor) {
     let isDark = false;
 
     // CSS injection.
+    // Body becomes dark when .dark class is applied on the body
+    // This CSS will be injected to the editor contents document at init (it's an iframe)
     let darkmodeCss = 'body.dark { background: #222; color: #FFD; }';
     editor.contentStyles.push(darkmodeCss);
 
@@ -34,9 +36,16 @@ tinymce.PluginManager.add('my_plugin_darkmode', function (editor) {
         icon: 'moon',
         tooltip: 'Dark mode',
         onclick: function () {
+            // First, we invert the memorized enabling status (true = Dark mode enabled)
             isDark = !isDark;
 
+            // 'this' here stands for the button
+            // Toggles 'active' aspect of the button
             this.active(isDark)
+
+            // Adds class .dark to the editor contents body (see CSS above)
+            // TinyMCE embeds a JQuery-like library, called DOMUtils :
+            // See https://www.tiny.cloud/docs-4x/api/tinymce.dom/tinymce.dom.domutils/
             editor.dom.toggleClass(editor.getBody(), 'dark', isDark);
         }
     });
